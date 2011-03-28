c547ffb4a0cc5b64fd695ac84e21c48dd597852a
Ed Finkler <coj@funkatron.com>
2011-03-28 17:14:19 -0400

    remove unused stuff

--- custom/AllFiles.php
+++ custom/AllFiles.php
@@ -4,9 +4,6 @@ define('FRAPI_CACHE_ADAPTER', 'apc');
 
 
 // Use the constant CUSTOM_MODEL to access the custom model directory
-require CUSTOM_MODEL . '/Spaz/Thumb.php';
 require CUSTOM_MODEL . '/Spaz/Url.php';
-require CUSTOM_MODEL . '/Frapi/Limit.php';
-require CUSTOM_MODEL . '/AppSTW.php';
 
 require dirname(__FILE__) . DIRECTORY_SEPARATOR.'helpers.php';
