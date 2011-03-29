48bb0ffb71245695e5ded8012446e89952912467
Ed Finkler <coj@funkatron.com>
2011-03-28 22:53:36 -0400

    Added avatar retrieval methods

---
+++ custom/Action/Avatar.php
@@ -0,0 +1,169 @@
+<?php
+
+/**
+ * Action Avatar 
+ * 
+ * Array
+ * 
+ * @link http://getfrapi.com
+ * @author Frapi <frapi@getfrapi.com>
+ * @link /avatar/:service/:userid
+ */
+class Action_Avatar extends Frapi_Action implements Frapi_Action_Interface
+{
+
+    /**
+     * Required parameters
+     * 
+     * @var An array of required parameters.
+     */
+    protected $requiredParams = array(
+        'service',
+        'userid'
+        );
+
+    /**
+     * The data container to use in toArray()
+     * 
+     * @var A container of data to fill and return in toArray()
+     */
+    private $data = array();
+
+    /**
+     * To Array
+     * 
+     * This method returns the value found in the database 
+     * into an associative array.
+     * 
+     * @return array
+     */
+    public function toArray()
+    {
+        $this->data['service'] = $this->getParam('service', self::TYPE_OUTPUT);
+        $this->data['userid'] = $this->getParam('userid', self::TYPE_OUTPUT);
+        return $this->data;
+    }
+
+    /**
+     * Default Call Method
+     * 
+     * This method is called when no specific request handler has been found
+     * 
+     * @return array
+     */
+    public function executeAction()
+    {
+        $valid = $this->hasRequiredParameters($this->requiredParams);
+        if ($valid instanceof Frapi_Error) {
+            return $valid;
+        }
+        
+        return $this->toArray();
+    }
+
+    /**
+     * Get Request Handler
+     * 
+     * This method is called when a request is a GET
+     * 
+     * @return array
+     */
+    public function executeGet()
+    {
+        $valid = $this->hasRequiredParameters($this->requiredParams);
+        if ($valid instanceof Frapi_Error) {
+            return $valid;
+        }
+
+        $service = $this->getParam('service');
+        $userid = $this->getParam('userid');
+        $redirect = $this->getParam('redirect');
+
+        $av = new SpazAvatar();
+        $url = $av->retrieve($service, $userid);
+        
+        if (!$url) {
+            throw new Frapi_Error('ERROR_RETRIEVING_AVATAR');
+        }
+        
+        if ($redirect && $redirect !== 'false') {
+            header("Location: $url");
+            exit;
+        }
+        
+        $this->data['url'] = $url;
+        
+        return $this->toArray();
+    }
+
+    /**
+     * Post Request Handler
+     * 
+     * This method is called when a request is a POST
+     * 
+     * @return array
+     */
+    public function executePost()
+    {
+        $valid = $this->hasRequiredParameters($this->requiredParams);
+        if ($valid instanceof Frapi_Error) {
+            return $valid;
+        }
+        
+        return $this->toArray();
+    }
+
+    /**
+     * Put Request Handler
+     * 
+     * This method is called when a request is a PUT
+     * 
+     * @return array
+     */
+    public function executePut()
+    {
+        $valid = $this->hasRequiredParameters($this->requiredParams);
+        if ($valid instanceof Frapi_Error) {
+            return $valid;
+        }
+        
+        return $this->toArray();
+    }
+
+    /**
+     * Delete Request Handler
+     * 
+     * This method is called when a request is a DELETE
+     * 
+     * @return array
+     */
+    public function executeDelete()
+    {
+        $valid = $this->hasRequiredParameters($this->requiredParams);
+        if ($valid instanceof Frapi_Error) {
+            return $valid;
+        }
+        
+        return $this->toArray();
+    }
+
+    /**
+     * Head Request Handler
+     * 
+     * This method is called when a request is a HEAD
+     * 
+     * @return array
+     */
+    public function executeHead()
+    {
+        $valid = $this->hasRequiredParameters($this->requiredParams);
+        if ($valid instanceof Frapi_Error) {
+            return $valid;
+        }
+        
+        return $this->toArray();
+    }
+
+
+}
+
---
+++ custom/Model/Spaz/Avatar.php
@@ -0,0 +1,78 @@
+<?php
+/**
+* 
+*/
+class SpazAvatar
+{
+    
+    function __construct()
+    {
+        $this->cache = Frapi_Cache::getInstance('apc');
+    }
+    
+    public function retrieve($service, $userid) {
+        
+        $ck = "avatar_".$service."_".$userid;
+        $url = $this->cache->get($ck);
+        
+        if (!$url) {
+            switch ($service) {
+                case 'identi.ca':
+                    $url = $this->retrieveIdentica($userid);
+                    break;
+
+                case 'twitter.com':
+                    $url = $this->retrieveTwitter($userid);
+                    break;
+
+                default:
+                    # code...
+                    break;
+            }
+            
+            $this->cache->add($ck, $url);
+            
+        }
+        
+        return $url;
+    }
+    
+    
+    protected function retrieveIdentica($userid)
+    {
+        
+        $api_url = "http://identi.ca/api/users/show/{$userid}.json";
+        
+        $http = new HttpRequest($api_url);
+        $http->send();
+        
+        if ($http->getStatusCode() == 200) {
+            $resp_obj = json_decode($http->getResponseBody());
+        }
+        
+        if ($resp_obj && isset($resp_obj->profile_image_url)) {
+            $url = $resp_obj->profile_image_url;
+            return $url;
+        } else {
+            return false;
+        }
+    }
+
+
+    protected function retrieveTwitter($userid)
+    {
+        
+        $api_url = "http://api.twitter.com/1/users/profile_image/{$userid}.json";
+        
+        $http = new HttpRequest($api_url, HttpRequest::METH_HEAD);
+        $http->send();
+        
+        $url = $http->getResponseHeader('location');
+        if ($url) {
+            return $url;
+        } else {
+            return false;
+        }
+
+    }
+}
