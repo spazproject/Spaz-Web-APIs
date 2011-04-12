2a8a96aceeeb2ae75884dd02c350e7e73088cecb
Ed Finkler <coj@funkatron.com>
2011-04-12 18:08:37 -0400

    Refactored and improved Resty, and expanded test suite

--- tests/lib/Resty.php
+++ tests/lib/Resty.php
@@ -12,6 +12,14 @@ class Resty
 	 */
 	protected $http;
 	
+	/**
+	 * undocumented variable
+	 *
+	 * @var string
+	 */
+	protected $debug = false;
+	
+	
 	function __construct()
 	{
 		$this->http = new HttpRequest();
@@ -21,35 +29,139 @@ class Resty
 	 * undocumented function
 	 *
 	 * @param int $method 
-	 * @param mixed $data 
+	 * @param mixed $querydata 
 	 * @param array $headers 
+	 * @param array $options 
 	 * @return array
 	 * @author Ed Finkler
 	 */
 	public function get($url, $querydata=null, $headers=null, $options=null) {
+		return $this->sendRequest($url, HttpRequest::METH_GET, $querydata, $headers, $options);
+	}
+	
+	public function post($url, $querydata=null, $headers=null, $options=null) {		
+		return $this->sendRequest($url, HttpRequest::METH_POST, $querydata, $headers, $options);
+	}
+	
+	public function put($url, $querydata=null, $headers=null, $options=null) {		
+		return $this->sendRequest($url, HttpRequest::METH_PUT, $querydata, $headers, $options);
+	}
+	
+	public function delete($url, $querydata=null, $headers=null, $options=null) {		
+		return $this->sendRequest($url, HttpRequest::METH_DELETE, $querydata, $headers, $options);
+	}
+	
+	
+	/**
+	 * undocumented function
+	 *
+	 * @param string $state 
+	 * @return void
+	 * @author Ed Finkler
+	 */
+	public function enableDebugging($state=false) {
+		$state = (bool)$state;
 		
-		$this->http->setMethod(HttpRequest::METH_GET);
+		$this->debug = $state;
+	}
+	
+	
+	/**
+	 * undocumented function
+	 *
+	 * @param string $msg 
+	 * @return void
+	 * @author Ed Finkler
+	 */
+	protected function log($msg) {
 		
+		if (!$this->debug) { return; }
+		
+		echo date(DateTime::RFC822) . " :: ";
+		
+		if (is_string($msg)) {
+			echo "{$msg}\n";
+		} else {
+			var_dump($msg);
+			echo "\n";
+		}
+	}
+	
+	
+	/**
+	 * undocumented function
+	 *
+	 * @param string $url 
+	 * @param string $method 
+	 * @param string $querydata 
+	 * @param string $headers 
+	 * @param string $options 
+	 * @return array
+	 * @author Ed Finkler
+	 */
+	public function sendRequest($url, $method=HttpRequest::METH_GET, $querydata=null, $headers=null, $options=null) {
+		
+		$this->log($url);
 		$this->http->setUrl($url);
 		
+		$this->log($method);
+		$this->http->setMethod($method);
+		
+		$this->log($querydata);
 		if (isset($querydata)) {
 			$this->http->setQueryData($querydata);
 		}
 		
+		$this->log($headers);
 		if (isset($headers)) {
 			$this->http->setHeaders($headers);
 		}
 		
+		$this->log($options);
 		if (isset($options)) {
 			$this->http->setOptions($options);
 		}
 		
+		$this->log("Sending…");
 		$this->http->send();
 		
+		$this->log("Getting response…");
 		$resp = $this->http->getResponseData();
+		$this->log($resp);
+		
+		$this->log("Processing response body…");
+		$resp = $this->processResponseBody($resp);
+		$this->log($resp['body']);
 		
 		return $resp;
 	}
 	
+	
+	/**
+	 * undocumented function
+	 *
+	 * @param string $resp 
+	 * @return void
+	 * @author Ed Finkler
+	 */
+	protected function processResponseBody($resp) {
+		
+		if (strpos($resp['headers']['Content-Type'], 'json') !== FALSE) {
+			
+			$this->log("Response body is JSON");
+			$resp['body'] = json_decode($resp['body']);
+			
+		} elseif (strpos($resp['headers']['Content-Type'], 'xml') !== FALSE) {
+			
+			$this->log("Response body is XML");
+			$resp['body'] = new SimpleXMLElement($resp['body']);
+			
+		} else {
+			$this->log("Response body wasn't XML or JSON; not parsed");
+		}
+		
+		return $resp;
+		
+	}
 	
 }
--- tests/run.php
+++ tests/run.php
@@ -13,10 +13,10 @@ class SpazApiTests extends UnitTestCase
 	public function testOfTime() {
 		
 		$rest = new Resty();
-		
+
 		$resp = $rest->get(self::API_URL."/time");
 		
-		$timeobj = json_decode($resp['body']);
+		$timeobj = $resp['body'];
 		
 		$this->assertNotNull($timeobj->time);
 		$this->assertNotNull($timeobj->time->tz);
@@ -26,10 +26,69 @@ class SpazApiTests extends UnitTestCase
 		$this->assertNotNull($timeobj->time->second);
 		
 		
-		$resp = $rest->get(self::API_URL."/time/?tz=America/Indianapolis");
-		$timeobj = json_decode($resp['body']);
+		$resp = $rest->get(
+			self::API_URL."/time/",
+			array('tz'=>'America/Indianapolis')
+		);
+		
+		$timeobj = $resp['body'];
 		$this->assertTrue($timeobj->time->tz === 'America/Indianapolis');
 		
+	}
+	
+	
+	public function testOfUrlInfo() {
+		$info_url = "http://getspaz.com";
+		
+		$rest = new Resty();
+		
+		$resp = $rest->get(
+			self::API_URL."/url/info",
+			array('url'=>$info_url)
+		);
+		
+		$url_info = $resp['body'];
+		$headers  = $resp['headers'];
+
+		$this->assertEqual($url_info->resolved_url, $info_url);
+		$this->assertEqual($url_info->content_type, "text/html; charset=UTF-8");
+		$this->assertEqual($url_info->http_code, 200);
+
+		$this->assertEqual($headers['Content-Type'], 'application/json; charset=utf-8');
+	}
+
+	
+	public function testOfUrlTitle() {
+		$info_url = "http://getspaz.com";
+		
+		$rest = new Resty();
+		
+		$resp = $rest->get(
+			self::API_URL."/url/title",
+			array('url'=>$info_url)
+		);
+		
+		$url_info = $resp['body'];
+		$headers  = $resp['headers'];
+
+		$this->assertEqual($url_info->url, "http://getspaz.com");
+		$this->assertEqual(strpos($url_info->title, "Spaz: A Twitter, Identi.ca and StatusNet client"), 0);
+	}
+	
+	
+	public function testOfUrlResolve() {
+		$rest = new Resty();
+		
+		$resp = $rest->get(
+			self::API_URL."/url/resolve",
+			array('url'=>'http://j.mp/spazwebosbeta')
+		);
+		
+		$resolved = $resp['body'];
+
+		$this->assertEqual($resolved->passed_url, "http://j.mp/spazwebosbeta");
+		$this->assertEqual($resolved->final_url, "https://developer.palm.com/appredirect/?packageid=com.funkatron.app.spaz-beta&applicationid=4928");
+		$this->assertTrue($resolved->redirects >= 1);
 	}
 	
 }
